3120a49bfe8e2cb78dc7d8da83d198499395dead
Paul Thomas <dt01pqt_pt@yahoo.com@yahoo.com>
2015-03-10 10:11:53 +0000

    remove EOM locale

--- class.replybyemail.plugin.php
+++ class.replybyemail.plugin.php
@@ -4,7 +4,7 @@
 $PluginInfo['ReplyByEmail'] = array(
    'Name' => 'ReplyByEmail',
    'Description' => 'Allows users to reply by email to discussions, wall post, and messages using the replypush.com service',
-   'Version' => '0.1.13b',
+   'Version' => '0.1.14b',
    'Author' => "Paul Thomas",
    'SettingsUrl' => 'settings/replybyemail',
    'RequiredApplications' => array('Vanilla' => '2.1'),
